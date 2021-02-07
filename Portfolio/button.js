99ebbc9243b152a4a755066f346e7c615666c366
hungdng149 <77676086+hungdng149@users.noreply.github.com>
2021-02-06 22:43:59 -0500

    Add files via upload

---
+++ Portfolio/button.js
@@ -0,0 +1,32 @@
+
+function peerTAbutton() {
+    document.getElementById("job_name").innerHTML = "Peer Teaching Assistant at University of Cincinnati";
+    document.getElementById("job_duty_1").innerHTML = "Assist students with difficult aspects of course material";
+    document.getElementById("job_duty_2").innerHTML = "Prepare lesson plans and activities for recitation";
+    document.getElementById("job_duty_3").innerHTML = "Develop and strengthen effective studying strategies for students";
+    document.getElementById("job_duty_4").innerHTML = "Conduct drop-in tutoring sessions with students weekly";  
+}
+
+function DJJbutton() {
+    document.getElementById("job_name").innerHTML = "Data Engineer Co-op at The David J Joseph Company";
+    document.getElementById("job_duty_1").innerHTML = "Improved SQL stored procedures and get rid of technical debt to improve performance during peak hours.";
+    document.getElementById("job_duty_2").innerHTML = "Created and modified SSRS reports that suits other department's needs. ";
+    document.getElementById("job_duty_3").innerHTML = "Utilized Python in conjunction with SQL to create script that automatically finds, determine and geocode unique locations and import them into a SQL database.";
+    document.getElementById("job_duty_4").innerHTML = "Automating the location retrieval and geocoding process saved the company hundreds of man-hours of manually entering and searching for the data.";  
+}
+
+function FSoftbutton() {
+    document.getElementById("job_name").innerHTML = "Software Engineer Intern at FPT Software";
+    document.getElementById("job_duty_1").innerHTML = "Assisted in the creation and deployment of the company's local cloud storage setup. ";
+    document.getElementById("job_duty_2").innerHTML = "Handled communications between Clients and Developers to ensure requirements are properly met.";
+    document.getElementById("job_duty_3").innerHTML = "Designed and develop test case and did quality assurance testing with Java.";
+    document.getElementById("job_duty_4").innerHTML = "Trained on code refactoring and optimization to improve runtime.";  
+}
+
+function OneSightbutton() {
+    document.getElementById("job_name").innerHTML = "Data Engineer Co-op at OneSight Inc.";
+    document.getElementById("job_duty_1").innerHTML = "Cleaned up techincal debts and improve SQL procedures runtime";
+    document.getElementById("job_duty_2").innerHTML = "Implemented changes in AssetPanda and Meraki to facilitate tracking and managing hardware coming in and out";
+    document.getElementById("job_duty_3").innerHTML = "Developed a Python script used to crawl a website and build database upon the information in the website using MongoDB, scrapy, HTML and CSS";
+    document.getElementById("job_duty_4").innerHTML = "";  
+}
